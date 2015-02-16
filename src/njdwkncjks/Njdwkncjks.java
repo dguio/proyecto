8e8f2319658aced51ff7cab81a074862931cc8d4
david <david@batista>
2015-02-16 16:08:46 -0500

    version inicial

--- src/njdwkncjks/Njdwkncjks.java
+++ src/njdwkncjks/Njdwkncjks.java
@@ -16,6 +16,7 @@ public class Njdwkncjks {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("bjdkfnkjcb");
     }
     
 }
