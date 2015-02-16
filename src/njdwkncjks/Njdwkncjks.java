84df1acc5a4de1685dbaa797bc0a5d8dacbad9eb
Gustavo <Gustavo@Tavo>
2015-02-16 16:12:51 -0500

    Añadido colaboracion 1

--- src/njdwkncjks/Njdwkncjks.java
+++ src/njdwkncjks/Njdwkncjks.java
@@ -16,7 +16,7 @@ public class Njdwkncjks {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("bjdkfnkjcb");
+        System.out.println("Probando edicion de colaboaracion");
     }
     
 }
